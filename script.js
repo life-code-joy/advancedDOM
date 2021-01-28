f69d8f573472e1ed20d7b637e6deb9e95f702494
darren <darren__c@hotmail.com>
2021-01-28 18:35:02 +0900

    styles,attributes and classes

--- script.js
+++ script.js
@@ -91,3 +91,39 @@ console.log(logo.designer);
 
 logo.setAttribute('company', 'Bankist');
 console.log(logo);
+
+const twitLink = document.querySelector('.twitter-link');
+console.log(twitLink);
+
+console.log(twitLink.href);
+console.log(twitLink.getAttribute('href'));
+
+// * using the regular property will giv you the absolute path
+// * using getAttribute will gwt the attribute that is in the HTML
+const menuLinks = document.querySelector('.nav__links a');
+console.log(menuLinks);
+console.log(menuLinks.href);
+
+console.log(menuLinks.getAttribute('href'));
+
+// Data attributes - special attributes
+// they start with word 'data' then whatever we want ex. data-version-number="3"
+
+// Let's add one to our image
+// data-version-number="3"
+// in the HTML we use hypens and in the JS we use camel-case
+// they are always stored in the dataset object
+// use data attributes quite a lot when we work with the UI to store info in the HTML about the UI
+// we will use a lot throughout this project and course
+console.log(logo.dataset.versionNumber); // 3.0
+
+// classes  add,remove,toggle,contains
+//these are the four you really need to know
+//ex
+
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c', 'j');
+
+//you could - logo.className = "jonus" BUT don't do this because it'll overwrite everything and you can only add 1 classname. Use the above methods <!DOCTYPE html>
